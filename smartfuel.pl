5f1acba32c92e946f66f1387254ab4cfd3a0d6af
enoch2-hub <enoch.perera.coding2@gmail.com>
2026-04-17 15:44:04 +0530

    add contribution file

---
+++ smartfuel.pl
@@ -0,0 +1,74 @@
+% ==========================================
+% GLOBAL SYSTEM SETTINGS (The "Today" Facts)
+% ==========================================
+% Change these facts to test different scenarios (e.g., date 13 or holiday false)
+
+
+
+
+
+
+
+
+% ==========================================
+% SECTOR DATA (Fisheries Records)
+% ==========================================
+% vessel(VesselID, LastFuelingDate)
+
+
+
+
+
+
+
+
+% ==========================================
+% CORE LOGIC: VEHICLE ELIGIBILITY (Everyday Users)
+% ==========================================
+% Rule for Petrol
+
+
+
+
+
+
+% Rule for Diesel (Always requires QR)
+
+
+
+
+
+
+
+
+% ==========================================
+% CORE LOGIC: FISHERIES ALLOCATION
+% ==========================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% ==========================================
+% INTERACTIVE USER INTERFACE
+% ==========================================
+% This starts the application
+
+
+
+
+
+
+
+
+
+
